7858b4f63f70821348d6f245068f87095e2ad509
V1def <vaniya8871@gmail.com>
2022-06-08 16:46:23 +0300

    Create base bot

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,3 +1,51 @@
+/*
+ * Copyright © 2022 Durudex
+
+ * This file is part of Durudex: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * Durudex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Durudex. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// A function that running the bot.
+func main() {
+	// Create a new Discord session using the provided bot token.
+	session, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open a websocket connection to Discord and begin listening.
+	if err := session.Open(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Quit in application.
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	// Close the websocket connection to Discord.
+	if err := session.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
